7526732043fa611a86c0fbc6a0b88cb8e3e5d5d1
Muhammad Abid <110349495+mabid-se@users.noreply.github.com>
2023-05-17 00:41:38 +0500

    Created the main navbar according to figma design (#3)
    Co-authored-by: Muhammad Abid <abid@thesgn.co>

---
+++ src/components/navbar/MainNavbar-styles.js
@@ -0,0 +1,95 @@
+import { makeStyles } from "@mui/styles";
+
+import { myTheme } from "../../theme";
+
+export const useStyles = makeStyles((theme) => ({
+  logoContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    gap: 4,
+  },
+  logoText: {
+    color: myTheme.palette.white.four,
+    fontWeight: "medium",
+    fontFamily: "Rubik",
+    letterSpacing: 1,
+  },
+  menuItemsContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  indMenuItem: {
+    fontFamily: "Rubik",
+    fontWeight: "medium",
+    textTransform: "capitalize",
+    color: myTheme.palette.white.four,
+    textDecoration: "none",
+    "&:hover": { color: myTheme.palette.yellow.two },
+  },
+  indMenuItemActive: {
+    color: myTheme.palette.yellow.two,
+    fontWeight: "bold",
+    textDecoration: "underline",
+    textDecorationThickness: "2px",
+    textUnderlineOffset: "30px",
+  },
+  reqQuoteContainer: {
+    backgroundColor: myTheme.palette.white.four,
+    cursor: "pointer",
+  },
+  reqQuoteText: {
+    color: myTheme.palette.black.three,
+    textDecoration: "none",
+    textTransform: "capitalize",
+    fontWeight: "medium",
+  },
+  drawerContainer: {
+    textTransform: "capitalize",
+    textAlign: "center",
+  },
+  reqQuoteBtnDrawer: {
+    background: `linear-gradient(94.06deg, #FFB629 -1.21%, #FFDA56 58.66%, #FFD7A6 116.84%)`,
+    borderRadius: 0,
+    padding: "1em 1.5em",
+    color: myTheme.palette.black.three,
+    fontWeight: "medium",
+    textDecoration: "none",
+    textTransform: "capitalize",
+    "&:hover": { background: myTheme.palette.white.four },
+  },
+  drawerItemsCont: {
+    padding: "1.2em 0",
+    color: myTheme.palette.white.four,
+    borderBottom: "2px solid",
+    fontWeight: "medium",
+  },
+  indDrawerItem: {
+    fontFamily: "Rubik",
+    fontWeight: "medium",
+    textTransform: "capitalize",
+    color: myTheme.palette.white.four,
+    textDecoration: "none",
+    "&:hover": { color: myTheme.palette.yellow.two },
+  },
+  indDrawerItemActive: {
+    fontWeight: "bold",
+    color: myTheme.palette.yellow.two,
+  },
+  socialIconCont: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "1em",
+    gap: 15,
+  },
+  indSocialIcon: {
+    color: myTheme.palette.white.four,
+    "&:hover": { color: myTheme.palette.yellow.two },
+  },
+}));
